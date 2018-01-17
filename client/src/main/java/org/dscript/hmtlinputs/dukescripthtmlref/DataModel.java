92196a0ebe8839d3e16b93634db85785b4757aeb
tonie <tonie@DESKTOP-9727Q2Q>
2018-01-17 15:03:29 +0100

    progressbar example

--- client/src/main/java/org/dscript/hmtlinputs/dukescripthtmlref/DataModel.java
+++ client/src/main/java/org/dscript/hmtlinputs/dukescripthtmlref/DataModel.java
@@ -8,6 +8,7 @@ import java.util.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.stage.FileChooser;
+import net.java.html.BrwsrCtx;
 import net.java.html.json.ComputedProperty;
 import net.java.html.json.Function;
 import net.java.html.json.Model;
@@ -19,27 +20,44 @@ import net.java.html.json.OnPropertyChange;
 import org.dscript.hmtlinputs.dukescripthtmlref.js.PlatformServices;
 import org.dscript.hmtlinputs.dukescripthtmlref.js.TemplateRegistration;
 
-
-
 @Model(className = "Data", targetId = "", properties = {
-    @Property(name = "template", type = String.class),
-    @Property(name = "contentA", type = String.class),
-    @Property(name = "contentB", type = String.class), 
-    @Property(name = "contentC", type = String.class),
-    @Property(name = "errorMessage", type = String.class), 
-    @Property(name = "errorField", type = String.class),
-    @Property(name = "theaterReviews", type = TheaterReview.class, array = true),
-    @Property(name = "editedTheaterReview", type = TheaterReview.class),
-    @Property(name = "synesthesiaValues", type = String.class, array = true),
-    @Property(name = "ratingValues", type = int.class, array = true),
-    @Property(name = "directories", type = String.class, array = true),
-    @Property(name = "directoryValues", type = String.class, array = true),
-    @Property(name = "editedfiletitle", type= String.class),
-    @Property(name = "editeduploadedfile", type= String.class),
-    @Property(name = "uplfiles", type = UplFile.class, array=true),
-    @Property(name = "currentMenu", type = int.class),
-     @Property(name = "strActionWithDuration", type = String.class),
-})
+    @Property(name = "template", type = String.class)
+    ,
+    @Property(name = "contentA", type = String.class)
+    ,
+    @Property(name = "contentB", type = String.class)
+    , 
+    @Property(name = "contentC", type = String.class)
+    ,
+    @Property(name = "errorMessage", type = String.class)
+    , 
+    @Property(name = "errorField", type = String.class)
+    ,
+    @Property(name = "theaterReviews", type = TheaterReview.class, array = true)
+    ,
+    @Property(name = "editedTheaterReview", type = TheaterReview.class)
+    ,
+    @Property(name = "synesthesiaValues", type = String.class, array = true)
+    ,
+    @Property(name = "ratingValues", type = int.class, array = true)
+    ,
+    @Property(name = "directories", type = String.class, array = true)
+    ,
+    @Property(name = "directoryValues", type = String.class, array = true)
+    ,
+    @Property(name = "editedfiletitle", type = String.class)
+    ,
+    @Property(name = "editeduploadedfile", type = String.class)
+    ,
+    @Property(name = "uplfiles", type = UplFile.class, array = true)
+    ,
+    @Property(name = "currentMenu", type = int.class)
+    ,
+     @Property(name = "strActionWithDuration", type = String.class)
+    ,
+     @Property(name = "showProgress", type = boolean.class)
+    ,
+     @Property(name = "progress", type = int.class),})
 
 final class DataModel {
 
@@ -47,21 +65,18 @@ final class DataModel {
     private static Closeable a;
     private static Closeable b;
     private static Closeable f;
-    
-    
-    
-    @Model(className="UplFile", properties = {
-        @Property(name = "origFileName", type = String.class),
-        @Property(name = "targetFileName", type = String.class),
-        @Property(name = "userName4File", type = String.class),
-    })
-    
-    public static class UplFileVMD{
-        
+
+    @Model(className = "UplFile", properties = {
+        @Property(name = "origFileName", type = String.class)
+        ,
+        @Property(name = "targetFileName", type = String.class)
+        ,
+        @Property(name = "userName4File", type = String.class),})
+
+    public static class UplFileVMD {
+
     }
-    
-    
-    
+
     @Function
     public static void uploadFile(Data model) {
 
@@ -85,9 +100,8 @@ final class DataModel {
         }
 
     }
-    
-    
-     public static String showFileDialog(String strTitel, String strAllowedFileExts) {
+
+    public static String showFileDialog(String strTitel, String strAllowedFileExts) {
         FileChooser fileChooser = new FileChooser();
         fileChooser.setTitle(strTitel);
         String[] split = strAllowedFileExts.split(" ");
@@ -105,70 +119,71 @@ final class DataModel {
         } else {
             return null;
         }
-    }    
-    
-    
-    
+    }
 
     public static void init() {
-        
+
         ui = new Data();
         Models.toRaw(ui);
         a = TemplateRegistration.register("a", "a.html");
         b = TemplateRegistration.register("b", "b.html");
         f = TemplateRegistration.register("f", "file.html");
-        
+
         // Variable errorField is used in form: must be available before template ist set.
         ui.setErrorField("nope");
         ui.setTemplate("a");
         //  Highlighting in the mail menue
         ui.setCurrentMenu(1);
-        
+
         ui.setStrActionWithDuration("Noch nicht ausgeführt.");
-        
+
         ui.setContentA("Please be first and rate your last theater visit!");
         ui.setContentB("This is Content for Template B!");
         ui.setContentC("This is Content for Template C!");
-        
+
         ui.getRatingValues().add(1);
         ui.getRatingValues().add(2);
         ui.getRatingValues().add(3);
         ui.getRatingValues().add(4);
         ui.getRatingValues().add(5);
-        
+
         ui.getSynesthesiaValues().add("red");
         ui.getSynesthesiaValues().add("green");
         ui.getSynesthesiaValues().add("blue");
         ui.getSynesthesiaValues().add("orange");
         ui.getSynesthesiaValues().add("yellow");
-        
-        ui.getDirectoryValues().add("Target A 1");       
+
+        ui.getDirectoryValues().add("Target A 1");
         ui.getDirectoryValues().add("Target A 2");
         ui.getDirectoryValues().add("Target B 1");
         ui.getDirectoryValues().add("Target B 2");
     }
-    
+
+    static BrwsrCtx context;
+
     /**
      * Called when the page is ready.
      */
     static void onPageLoad() throws Exception {
+        context = BrwsrCtx.findDefault(Data.class);
         init();
         ui.applyBindings();
     }
 
     @Model(className = "TheaterReview", properties = {
-        @Property(name = "performance", type = String.class), 
-        @Property(name = "dateOfVisit", type = String.class), 
-        @Property(name = "content", type = String.class),   
-        @Property(name = "synesthesia", type = String.class, array = true),
-        @Property(name = "rating", type = int.class),
-    })
-    
-    public static class TheaterReviewVMD {
-        
-    }
-    
+        @Property(name = "performance", type = String.class)
+        , 
+        @Property(name = "dateOfVisit", type = String.class)
+        , 
+        @Property(name = "content", type = String.class)
+        ,   
+        @Property(name = "synesthesia", type = String.class, array = true)
+        ,
+        @Property(name = "rating", type = int.class),})
 
+    public static class TheaterReviewVMD {
+
+    }
 
     @Function
     public static void setA(Data model) {
@@ -181,13 +196,13 @@ final class DataModel {
         model.setCurrentMenu(2);
         model.setTemplate("b");
     }
-    
+
     @Function
     public static void setF(Data model) {
         model.setCurrentMenu(3);
         model.setTemplate("f");
     }
-    
+
     @Function
     public static void savePerformanceRatingSetB(Data model) {
 
@@ -216,30 +231,52 @@ final class DataModel {
         }
 
     }
-    
+
     @Function
-     public static void actionExitNow() {
+    public static void actionExitNow() {
         String strMethodName1 = DataModel.class.getSimpleName() + ".actionExitNow";
 
-                    String strAppShutDownMessage = "Shutdown application now.";
-                    System.out.println(strAppShutDownMessage);
-                    System.exit(0);
+        String strAppShutDownMessage = "Shutdown application now.";
+        System.out.println(strAppShutDownMessage);
+        System.exit(0);
     }
 
-     
     @Function
-    public static void doSomethingWithLongDuration(Data model) {
+    public static void doSomethingWithLongDuration(final Data model) {
         System.out.println("Starte lang dauernde Activity");
-        try {
-            Thread.sleep(5000); //5 sekunden schlafen
-            // hier soll auf der Seite ein Fortschrittsbalken  angezeigt werden
-            model.setStrActionWithDuration("Die langdauernde Aktion wurde ausgeführt.");
-            
-        } catch (InterruptedException ex) {
-            Logger.getLogger(DataModel.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        setB(model);
+        model.setShowProgress(true);
+        model.setProgress(0);
+        Runnable backgroundTask = new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 100; i++) {
+                    try {
+                        Thread.sleep(50);
+                        context.execute(new Runnable() {
+                            @Override
+                            public void run() {
+                                model.setProgress(model.getProgress() + 1);
+                            }
+                        });
+
+                    } catch (InterruptedException ex) {
+                        Logger.getLogger(DataModel.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+                context.execute(new Runnable() {
+                    @Override
+                    public void run() {
+                        model.setStrActionWithDuration("Die langdauernde Aktion wurde ausgeführt.");
+                    }
+                });
+
+            }
+        };
+        new Thread(backgroundTask).start();
+
+//        setB(model);
         System.out.println("Beende dauernde Activity");
+//        model.setShowProgress(false);
     }
-     
+
 }
